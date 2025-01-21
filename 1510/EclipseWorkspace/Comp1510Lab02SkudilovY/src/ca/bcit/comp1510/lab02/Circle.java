49aec0d422f5f0701cb14e7ab723d7982c4edbc6
Yehor <yskudilov@gmail.com>
2025-01-21 15:21:32 -0800

    1

---
+++ 1510/EclipseWorkspace/Comp1510Lab02SkudilovY/src/ca/bcit/comp1510/lab02/Circle.java
@@ -0,0 +1,19 @@
+
+package ca.bcit.comp1510.lab02;
+
+/**
+ * Circle.Calculates some important values for a Circle
+ * @Author Yehor S.
+ * @Version 2025
+ */
+public class Circle {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
