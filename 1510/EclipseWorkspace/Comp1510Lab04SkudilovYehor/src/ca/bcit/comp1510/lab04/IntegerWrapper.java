c9904b70d358bf452523ebf55c759fc8b769f902
Yehor <yskudilov@gmail.com>
2025-02-04 15:36:33 -0800

    1

---
+++ 1510/EclipseWorkspace/Comp1510Lab04SkudilovYehor/src/ca/bcit/comp1510/lab04/IntegerWrapper.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp1510.lab04;
+
+/**
+ * IntegerWrapper.
+ * @author Yehor S.
+ * @version 2025
+ */
+public class IntegerWrapper {
+/**
+ * Runs the program.
+ * @param args unused.
+ */
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
